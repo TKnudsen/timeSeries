7f00994701107f26c176a0e00762ffa6a07101fb
David Sessler <dsessler@igd.fraunhofer.de>
2017-05-22 13:43:41 +0200

    added functionality for storing/loading lists of time series

--- src/main/java/com/github/TKnudsen/timeseries/operations/io/json/JSONWriter.java
+++ src/main/java/com/github/TKnudsen/timeseries/operations/io/json/JSONWriter.java
@@ -2,11 +2,13 @@ package com.github.TKnudsen.timeseries.operations.io.json;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.TKnudsen.timeseries.data.ITimeSeries;
+import com.github.TKnudsen.timeseries.data.univariate.ITimeSeriesUnivariate;
 
 /**
  * <p>
@@ -26,73 +28,75 @@ import com.github.TKnudsen.timeseries.data.ITimeSeries;
  */
 public class JSONWriter {
 
-//	public static String writeToString(ITimeSeriesUnivariate ts) {
-//		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
-//
-//		String writeValueAsString;
-//		try {
-//			writeValueAsString = mapper.writeValueAsString(ts);
-//			return writeValueAsString;
-//		} catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//	}
-//
-//	public static void writeToFile(ITimeSeriesUnivariate ts, String fileName) {
-//		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
-//		mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-//
-//		// create dirs when necessary
-//		File file = new File(fileName);
-//		if (file.getParentFile() != null)
-//			file.getParentFile().mkdirs();
-//
-//		try {
-//			mapper.writeValue(file, ts);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		return;
-//	}
-//
-//	public static String writeToString(ITimeSeriesMultivariate timeSeriesMV) {
-//		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
-//
-//		String writeValueAsString;
-//		try {
-//			writeValueAsString = mapper.writeValueAsString(timeSeriesMV);
-//			return writeValueAsString;
-//		} catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//
-//	}
-//
-//	public static void writeToFile(ITimeSeriesMultivariate timeSeriesMV, String fileMV) {
-//		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
-//		mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-//
-//		// create dirs when necessary
-//		File file = new File(fileMV);
-//		if (file.getParentFile() != null)
-//			file.getParentFile().mkdirs();
-//
-//		try {
-//			mapper.writeValue(file, timeSeriesMV);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		return;
-//		
-//	}
+	// public static String writeToString(ITimeSeriesUnivariate ts) {
+	// ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
+	//
+	// String writeValueAsString;
+	// try {
+	// writeValueAsString = mapper.writeValueAsString(ts);
+	// return writeValueAsString;
+	// } catch (JsonProcessingException e) {
+	// e.printStackTrace();
+	// }
+	// return null;
+	// }
+	//
+	// public static void writeToFile(ITimeSeriesUnivariate ts, String fileName)
+	// {
+	// ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
+	// mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+	// mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+	//
+	// // create dirs when necessary
+	// File file = new File(fileName);
+	// if (file.getParentFile() != null)
+	// file.getParentFile().mkdirs();
+	//
+	// try {
+	// mapper.writeValue(file, ts);
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	//
+	// return;
+	// }
+	//
+	// public static String writeToString(ITimeSeriesMultivariate timeSeriesMV)
+	// {
+	// ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
+	//
+	// String writeValueAsString;
+	// try {
+	// writeValueAsString = mapper.writeValueAsString(timeSeriesMV);
+	// return writeValueAsString;
+	// } catch (JsonProcessingException e) {
+	// e.printStackTrace();
+	// }
+	// return null;
+	//
+	// }
+	//
+	// public static void writeToFile(ITimeSeriesMultivariate timeSeriesMV,
+	// String fileMV) {
+	// ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
+	// mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+	// mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+	//
+	// // create dirs when necessary
+	// File file = new File(fileMV);
+	// if (file.getParentFile() != null)
+	// file.getParentFile().mkdirs();
+	//
+	// try {
+	// mapper.writeValue(file, timeSeriesMV);
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	//
+	// return;
+	//
+	// }
 
-	
 	public static String writeToString(ITimeSeries ts) {
 		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
 
@@ -118,6 +122,23 @@ public class JSONWriter {
 
 		try {
 			mapper.writeValue(file, ts);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return;
+	}
+
+	public static void writeTSListToFile(List<ITimeSeriesUnivariate> tsList, String fileName) {
+		ObjectMapper mapper = ObjectMapperFactory.getTimeSeriesObjectMapper();
+
+		// create dirs when necessary
+		File file = new File(fileName);
+		if (file.getParentFile() != null)
+			file.getParentFile().mkdirs();
+
+		try {
+			mapper.writeValue(file, tsList);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
