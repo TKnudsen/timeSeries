6bff579612144620bd0abbe5ed9eb4dd50bb0fcf
jubernar <juergen.bernard@igd.fraunhofer.de>
2017-07-20 18:49:50 +0200

    added TimeSeriesComparisonTools

---
+++ src/main/java/com/github/TKnudsen/timeseries/operations/tools/TimeSeriesComparisonTools.java
@@ -0,0 +1,63 @@
+package com.github.TKnudsen.timeseries.operations.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.TKnudsen.ComplexDataObject.model.tools.MathFunctions;
+import com.github.TKnudsen.timeseries.data.univariate.ITimeSeriesUnivariate;
+
+/**
+ * <p>
+ * Title: TimeQuantizationTools
+ * </p>
+ * 
+ * <p>
+ * Description: little helpers that assess differences between time series.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 20127
+ * </p>
+ * 
+ * @author Juergen Bernard
+ */
+public class TimeSeriesComparisonTools {
+
+	/**
+	 * calculates the difference between the mean values of two time series.
+	 * 
+	 * @param timeSeries1
+	 * @param timeSeries2
+	 * @return
+	 */
+	public static double compareMeanValues(ITimeSeriesUnivariate timeSeries1, ITimeSeriesUnivariate timeSeries2) {
+		double means1 = TimeSeriesTools.getMean(timeSeries1);
+		double means2 = TimeSeriesTools.getMean(timeSeries2);
+
+		return Math.abs(means2 - means1);
+	}
+
+	/**
+	 * calculates the average difference of the value domains of two time series
+	 * for every common time stamp.
+	 * 
+	 * @param timeSeries1
+	 * @param timeSeries2
+	 * @return
+	 */
+	public static double compareValueDifferenceOnAverage(ITimeSeriesUnivariate timeSeries1, ITimeSeriesUnivariate timeSeries2) {
+
+		List<Double> valueDifferences = new ArrayList<>();
+		for (int i = 0; i < timeSeries1.size(); i++) {
+			Double v1 = timeSeries1.getValue(i);
+
+			Long timeStamp = timeSeries1.getTimestamp(i);
+			Double v2 = timeSeries2.getValue(timeStamp, false);
+
+			if (v1 != null && v2 != null && !Double.isNaN(v1) && !Double.isNaN(v2))
+				valueDifferences.add(Math.abs(v2 - v1));
+		}
+
+		return MathFunctions.getMean(valueDifferences);
+	}
+}
