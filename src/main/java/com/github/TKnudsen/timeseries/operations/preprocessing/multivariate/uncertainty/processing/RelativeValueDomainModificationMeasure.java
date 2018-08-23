32141d81d17ce853dbbed7fa99208e1ff88614e2
Christian Bors <bors@ifs.tuwien.ac.at>
2018-08-23 14:52:53 +0200

    update method for quantifying value uncertainty for numeric values

--- src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/uncertainty/processing/RelativeValueDomainModificationMeasure.java
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/uncertainty/processing/RelativeValueDomainModificationMeasure.java
@@ -2,10 +2,25 @@ package com.github.TKnudsen.timeseries.operations.preprocessing.multivariate.unc
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 import java.util.TreeMap;
 
+import org.apache.commons.math3.analysis.function.Sqrt;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.Covariance;
+
 import com.github.TKnudsen.ComplexDataObject.data.uncertainty.Double.NumericalUncertainty;
+import com.github.TKnudsen.ComplexDataObject.model.tools.StatisticsSupport;
 import com.github.TKnudsen.timeseries.data.multivariate.ITimeSeriesMultivariate;
+
+import smile.math.distance.MahalanobisDistance;
+import weka.core.matrix.Matrix;
+import weka.estimators.MahalanobisEstimator;
 
 /**
  * <p>
@@ -27,12 +42,19 @@ public class RelativeValueDomainModificationMeasure
 		extends TimeSeriesProcessingUncertaintyMeasure<ITimeSeriesMultivariate, NumericalUncertainty> {
 
 	private static Double epsilon = 0.000000001;
+	private Double samplingRate = 0.4;
+
+	public RelativeValueDomainModificationMeasure(ITimeSeriesMultivariate originalTS,
+			ITimeSeriesMultivariate processedTimeSeries, Double samplingRate) {
+		this(originalTS, processedTimeSeries);
+		this.samplingRate = samplingRate;
+	}
 
 	public RelativeValueDomainModificationMeasure(ITimeSeriesMultivariate originalTS,
 			ITimeSeriesMultivariate processedTimeSeries) {
 		super(originalTS, processedTimeSeries);
 	}
-	
+
 	public RelativeValueDomainModificationMeasure() {
 		super();
 	}
@@ -51,11 +73,60 @@ public class RelativeValueDomainModificationMeasure
 	public void calculateUncertaintyMeasure() {
 		uncertaintiesOverTime = new TreeMap<>();
 
-		for (Long timeStamp : getOriginalTimeSeries().getTimestamps()) {
-			List<Double> originalValues = getOriginalTimeSeries().getValue(timeStamp, false);
-			List<Double> modifiedValues = null;
+		int maxSampleSize = getOriginalTimeSeries().getTimestamps().size();
+		List<Double> sampleRelatives = new ArrayList<>();
+		Random samplingGenerator = new Random();
+		List<StatisticsSupport> samplingStatsList = new ArrayList<StatisticsSupport>();
+		
+		List<Double> originalValues;
+		List<Double> modifiedValues;
+		List<Double> relatives;
+		
+		for(int i = 0; i <= maxSampleSize*this.samplingRate; ++i) {
+			int randIdx = samplingGenerator.nextInt(maxSampleSize);
+			Long timeStamp = getOriginalTimeSeries().getTimestamp(randIdx);
+			originalValues = getOriginalTimeSeries().getValue(timeStamp, false);
+			modifiedValues = null;
 
-			List<Double> relatives = new ArrayList<>();
+			relatives = new ArrayList<>();
+			try {
+				modifiedValues = getProcessedTimeSeries().getValue(timeStamp, false);
+			} catch (Exception e) {
+				// System.err.println(
+				// getName() + ": unable to retrieve time stamp of original time series in
+				// modified time series");
+				// uncertaintiesOverTime.put(timeStamp, new NumericalUncertainty(relatives));
+				continue;
+			}
+			
+			ListIterator<Double> modIt = modifiedValues.listIterator();
+			ListIterator<Double> origIt = originalValues.listIterator();
+			while (origIt.hasNext()) {
+				int idx = origIt.nextIndex();
+				
+				StatisticsSupport stats = null;
+				if(!samplingStatsList.isEmpty() && idx < samplingStatsList.size())
+					stats = samplingStatsList.get(idx);
+				if(stats == null) {
+					stats = new StatisticsSupport(sampleRelatives);
+					samplingStatsList.add(idx, stats);
+				}
+				
+				Double origVal = origIt.next();
+				Double modVal = modIt.next();
+				
+				if(modVal == null)
+					stats.addValue(1.0);
+				else 
+					stats.addValue(getRelativeValue(origVal, modVal));
+			}
+		}
+		
+		for (Long timeStamp : getOriginalTimeSeries().getTimestamps()) {
+			originalValues = getOriginalTimeSeries().getValue(timeStamp, false);
+			modifiedValues = null;
+
+			relatives = new ArrayList<>();
 			try {
 				modifiedValues = getProcessedTimeSeries().getValue(timeStamp, false);
 			} catch (Exception e) {
@@ -66,34 +137,58 @@ public class RelativeValueDomainModificationMeasure
 				continue;
 			}
 
-			// TODO please validate
+
+			
+//			// TODO please validate
 			for (int i = 0; i < originalValues.size(); i++) {
-				if (modifiedValues == null)
-					relatives.add(1.0); // no value means 100% change
-				else if (modifiedValues.get(i) == null)
-					relatives.add(1.0); // no value means 100% change
-				else {
-					// relative change.
-					if (originalValues.get(i) == 0)
-						if (modifiedValues.get(i) != 0)
-							relatives.add(1.0);// from zero to something means 100% change. ARGHH
-						else
-							relatives.add(0.0);
-					else if (modifiedValues.get(i) == originalValues.get(i))
-						relatives.add(0.0);
-					else {
-						double rel = (modifiedValues.get(i) - originalValues.get(i)) / originalValues.get(i);
-						// if(Math.abs(rel) > 1) {
-						// System.out.println("Error, relative value larger than 1: " + rel);
-						// }
-						if (Math.abs(rel) < epsilon)
-							rel = 0.0;
-						relatives.add(rel);
-					}
+				//we assume that our distribution is normally distributed
+				StatisticsSupport currentStat = samplingStatsList.get(i);
+				if(currentStat.getVariance() > 0) {
+					NormalDistribution nd = new NormalDistribution(currentStat.getMean(), Math.sqrt(currentStat.getVariance()));
+					
+					Double originalValue = originalValues.get(i);
+					Double modifiedValue = modifiedValues.get(i);
+					if(modifiedValue == null)
+						relatives.add(1.0);
+					
+					// relative difference
+					Double relativeValue = getRelativeValue(originalValue, modifiedValue);
+					// normalized relative difference
+					Double normalizedRelDif = Math.abs(relativeValue - currentStat.getMean());
+					//cumulative probability
+					Double cumProb = nd.cumulativeProbability(normalizedRelDif);
+					// subtracted by 0.5 (due to this being highest density around mean
+					cumProb -= 0.5;
+					// take absolute, to compensate for small variations around mean
+					cumProb = Math.abs(cumProb);
+					
+					// absolute value of difference between cumulative prob. of relative difference, normalized by mean
+					relatives.add(Math.abs(cumProb));
+
+				} else {
+					relatives.add(0.0);
 				}
 			}
 
 			uncertaintiesOverTime.put(timeStamp, new NumericalUncertainty(relatives));
+		}
+	}
+
+	private double getRelativeValue(double originalValue, double modifiedValue) {
+		// relative change.
+		if (originalValue == 0)
+			if (modifiedValue != 0)
+				return 1.0;// from zero to something means 100% change. ARGHH
+			else
+				return 0.0;
+		else if (modifiedValue == originalValue)
+			return 0.0;
+		else {
+			double relativeValue = (modifiedValue - originalValue) / originalValue;
+
+			if (Math.abs(relativeValue) < epsilon)
+				return 0.0;
+			return relativeValue;
 		}
 	}
 
