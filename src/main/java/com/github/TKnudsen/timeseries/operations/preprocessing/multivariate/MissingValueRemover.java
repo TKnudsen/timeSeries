68ae72e68987c01a7a3d0fc213cc1dacabce76ab
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2018-09-26 17:57:01 +0100

    value uncertainty

---
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/MissingValueRemover.java
@@ -0,0 +1,75 @@
+package com.github.TKnudsen.timeseries.operations.preprocessing.multivariate;
+
+import java.util.List;
+
+import com.github.TKnudsen.ComplexDataObject.model.processors.IDataProcessor;
+import com.github.TKnudsen.ComplexDataObject.model.processors.complexDataObject.DataProcessingCategory;
+import com.github.TKnudsen.timeseries.data.multivariate.ITimeSeriesMultivariate;
+import com.github.TKnudsen.timeseries.operations.preprocessing.TimeSeriesProcessor;
+import com.github.TKnudsen.timeseries.operations.tools.TimeSeriesTools;
+
+/**
+ * <p>
+ * Title: MissingValueRemover
+ * </p>
+ * 
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2015-2018
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
+public class MissingValueRemover extends TimeSeriesProcessor<ITimeSeriesMultivariate> {
+
+	private double missingValueIndicator;
+
+	@SuppressWarnings("unused")
+	private MissingValueRemover() {
+		this.missingValueIndicator = Double.NaN;
+	}
+
+	public MissingValueRemover(double missingValueIndicator) {
+		this.missingValueIndicator = missingValueIndicator;
+	}
+
+	@Override
+	public void process(List<ITimeSeriesMultivariate> data) {
+		if (data.isEmpty())
+			throw new IllegalStateException("List<TimeSeries> is empty");
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i) == null)
+				throw new IllegalStateException("TimeSeries is null");
+			if (data.get(i).isEmpty())
+				throw new IllegalStateException("TimeSeries is empty");
+			process(data.get(i));
+		}
+	}
+
+	private void process(ITimeSeriesMultivariate data) {
+		for (int i = 0; i < data.size(); i++) {
+			for (Double d : data.getValue(i))
+				if (TimeSeriesTools.compareDoubles(missingValueIndicator, d)) {
+					data.removeTimeValue(i);
+					i--;
+
+					break;
+				}
+		}
+	}
+
+	@Override
+	public DataProcessingCategory getPreprocessingCategory() {
+		return DataProcessingCategory.DATA_CLEANING;
+	}
+
+	@Override
+	public List<IDataProcessor<ITimeSeriesMultivariate>> getAlternativeParameterizations(int count) {
+		return null;
+	}
+}
