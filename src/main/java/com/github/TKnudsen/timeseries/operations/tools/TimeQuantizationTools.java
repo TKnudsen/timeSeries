f2afa3357e8849d230abb6d1b11356d94f56226e
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-10-18 12:08:26 +0200

    changes to EquidistanceProcessor

--- src/main/java/com/github/TKnudsen/timeseries/operations/tools/TimeQuantizationTools.java
+++ src/main/java/com/github/TKnudsen/timeseries/operations/tools/TimeQuantizationTools.java
@@ -87,7 +87,7 @@ public class TimeQuantizationTools {
 	 * @return Sorted distribution of timeStamp distances
 	 */
 	public static SortedMap<Long, Integer> calculateQuantizationDistribution(List<Long> quantizationList) {
-		SortedMap<Long, Integer> quantizationDist = new TreeMap<Long, Integer>();		
+		SortedMap<Long, Integer> quantizationDist = new TreeMap<Long, Integer>();
 		for (long quantization : quantizationList) {
 			if (quantizationDist.containsKey(quantization)) {
 				quantizationDist.put(quantization, quantizationDist.get(quantization) + 1);
@@ -101,49 +101,51 @@ public class TimeQuantizationTools {
 	/**
 	 * 
 	 * @param quantizationList
-	 * @return List containing the best timeStamp distances for equidistance (most occurrences). 
-	 * This is a list because there could be several timeStamp distances with the same number of occurrences.
+	 * @return List with quantization suggestions, criterion: most occurrences. In
+	 *         future, other concepts/heuristics may also be considered (greatest
+	 *         common divisor, least common multiple, etc.)
 	 */
 	public static List<Long> guessQuantization(List<Long> quantizationList) {
 		SortedMap<Long, Integer> quantizationDist = calculateQuantizationDistribution(quantizationList);
 		List<Entry<Long, Integer>> quantizationDistEntryList = new ArrayList<>(quantizationDist.entrySet());
 		quantizationDistEntryList.sort(Collections.reverseOrder(Entry.comparingByValue()));
-				
+
 		List<Long> quantizationGuesses = new ArrayList<Long>();
 		if (quantizationDistEntryList.size() > 0) {
 			long firstGuess = quantizationDistEntryList.get(0).getKey();
 			long firstGuessOccurences = quantizationDistEntryList.get(0).getValue();
 			quantizationGuesses.add(firstGuess);
 			if (quantizationDistEntryList.size() > 1) {
-				int i = 1; 
-				while (i < quantizationDistEntryList.size() && quantizationDistEntryList.get(i).getValue() == firstGuessOccurences) {
+				int i = 1;
+				while (i < quantizationDistEntryList.size()
+						&& quantizationDistEntryList.get(i).getValue() == firstGuessOccurences) {
 					quantizationGuesses.add(quantizationDistEntryList.get(i).getKey());
 					i++;
 				}
 			}
-		}				
+		}
 		return quantizationGuesses;
 	}
-	
+
 	/**
 	 * 
 	 * @param timeStampIndex
 	 * @param quantizationList
 	 * @return timeStamp distance for the given timeStamp index
 	 */
-	public static long getQuantizationFromTimeStampIndex(int timeStampIndex, List<Long> quantizationList) {		
-		if(timeStampIndex > -1 && timeStampIndex < quantizationList.size()) {
-			return quantizationList.get(timeStampIndex);			
+	public static long getQuantizationFromTimeStampIndex(int timeStampIndex, List<Long> quantizationList) {
+		if (timeStampIndex > -1 && timeStampIndex < quantizationList.size()) {
+			return quantizationList.get(timeStampIndex);
 		}
-		return 0;		
+		return 0;
 	}
 
 	/**
 	 * 
 	 * @param quantizationGuesses
 	 * @param quantizationList
-	 * @return timeStamp offset index for equidistance.
-	 * The start of the timeStamp distance series with the most consecutive occurrences will be used
+	 * @return timeStamp offset index for equidistance. The start of the timeStamp
+	 *         distance series with the most consecutive occurrences will be used
 	 */
 	public static int guessTemporalOffset(List<Long> quantizationGuesses, List<Long> quantizationList) {
 
@@ -166,7 +168,7 @@ public class TimeQuantizationTools {
 			}
 
 			Map<Integer, Integer> startIndexCount = new HashMap<Integer, Integer>();
-			
+
 			for (int startIndex : startIndexList) {
 				startIndexCount.put(startIndex, 0);
 				for (int i = startIndex; i < quantizationList.size(); i++) {
