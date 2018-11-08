25ad36c4b77a07f6c72e2cee32d93e6b41af1da0
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-11-08 14:00:03 +0100

    added a pip parameter to scale-up the process

--- src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/PerceptuallyImportantPoints.java
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/PerceptuallyImportantPoints.java
@@ -39,18 +39,28 @@ public class PerceptuallyImportantPoints extends TimeSeriesProcessor<ITimeSeries
 
 	private int pipCount;
 
-	public PerceptuallyImportantPoints(int pipCount) {
-		if (pipCount < 2)
-			throw new IllegalArgumentException("PIP: parameter value <2");
+	/**
+	 * In the iterative buttom-up process a next time stamp has to be identified to
+	 * be part of the pip set in every iteration. A selection criterion is used that
+	 * assesses the interestingness of remaining time stamps AMONG ALL DIMENSIONS.
+	 * 
+	 * The speedUpFactor is Used to steer the length of candidates (time stamps) to
+	 * be pipped next. The speedUpFactor 1.0 means that every time stamp is
+	 * considered in the rankings. Accordingly, the value of 100.0f means that 1% of
+	 * the candidates is considered.
+	 */
+	private float speedUpFactor;
 
-		this.pipCount = pipCount;
+	public PerceptuallyImportantPoints(int pipCount) {
+		this(pipCount, 1.0f);
 	}
 
-	public void setPipCount(int pipCount) {
+	public PerceptuallyImportantPoints(int pipCount, float speedUpFactor) {
 		if (pipCount < 2)
 			throw new IllegalArgumentException("PIP: parameter value <2");
 
 		this.pipCount = pipCount;
+		this.speedUpFactor = speedUpFactor;
 	}
 
 	@Override
@@ -78,19 +88,27 @@ public class PerceptuallyImportantPoints extends TimeSeriesProcessor<ITimeSeries
 
 			Map<Long, Integer> electionCounts = new HashMap<>();
 
+			// to speed-up the process the ranking size (number of candidat timestams) can
+			// be reduced.
+			// every remaining time stamp is considered
+			int rankCount = data.size() - pipTimeStamps.size();
+			if (speedUpFactor > 1.0)
+				rankCount /= speedUpFactor;
+
 			for (int dim = 0; dim < data.getDimensionality(); dim++) {
 				Ranking<EntryWithComparableKey<Double, Long>> dimensionRanking = com.github.TKnudsen.timeseries.operations.preprocessing.univariate.PerceptuallyImportantPoints
-						.calculateNextPipCandidates(data.getTimeSeries(dim), pipTimeStamps);
+						.calculateNextPipCandidates(data.getTimeSeries(dim), pipTimeStamps, rankCount);
 
-				// assign points to every time stamp according to the given dimension-based
-				// ranking
-
+				// assign score points to every time stamp,
+				// according to the given dimension-based rankings
 				int i = 0;
 				for (EntryWithComparableKey<Double, Long> pipCandidate : dimensionRanking) {
 					if (electionCounts.get(pipCandidate.getValue()) == null)
 						electionCounts.put(pipCandidate.getValue(), 0);
 
-					electionCounts.put(pipCandidate.getValue(), electionCounts.get(pipCandidate.getValue()) + i++);
+					int points = (data.size() - pipTimeStamps.size()) - (dimensionRanking.size() - i);
+					electionCounts.put(pipCandidate.getValue(), electionCounts.get(pipCandidate.getValue()) + points);
+					i++;
 				}
 			}
 
@@ -131,6 +149,21 @@ public class PerceptuallyImportantPoints extends TimeSeriesProcessor<ITimeSeries
 
 	public int getPipCount() {
 		return pipCount;
+	}
+
+	public void setPipCount(int pipCount) {
+		if (pipCount < 2)
+			throw new IllegalArgumentException("PIP: parameter value <2");
+
+		this.pipCount = pipCount;
+	}
+
+	public float getSpeedUpFactor() {
+		return speedUpFactor;
+	}
+
+	public void setSpeedUpFactor(float speedUpFactor) {
+		this.speedUpFactor = speedUpFactor;
 	}
 
 }
