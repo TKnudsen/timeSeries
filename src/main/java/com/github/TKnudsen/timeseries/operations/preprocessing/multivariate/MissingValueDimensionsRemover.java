a7a92219ca125496a3d0d088c4d01065d2e2323f
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-12-05 15:13:26 +0100

    MissingValueDimensionsRemover

---
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/MissingValueDimensionsRemover.java
@@ -0,0 +1,105 @@
+package com.github.TKnudsen.timeseries.operations.preprocessing.multivariate;
+
+import com.github.TKnudsen.ComplexDataObject.model.processors.IDataProcessor;
+import com.github.TKnudsen.ComplexDataObject.model.processors.ParameterSupportTools;
+import com.github.TKnudsen.ComplexDataObject.model.processors.complexDataObject.DataProcessingCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.TKnudsen.timeseries.data.multivariate.ITimeSeriesMultivariate;
+import com.github.TKnudsen.timeseries.data.univariate.ITimeSeriesUnivariate;
+import com.github.TKnudsen.timeseries.operations.preprocessing.TimeSeriesProcessor;
+import com.github.TKnudsen.timeseries.operations.tools.TimeSeriesTools;
+
+/**
+ * 
+ * TimeSeries
+ *
+ * Copyright: (c) 2015-2018 Juergen Bernard,
+ * https://github.com/TKnudsen/TimeSeries<br>
+ * 
+ * MissingValueDimensionsRemover is a pre-processing routine that removes
+ * dimensions with more % missing values as defined by a threshold
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
+public class MissingValueDimensionsRemover extends TimeSeriesProcessor<ITimeSeriesMultivariate> {
+
+	private double missingValueIndicator;
+
+	/**
+	 * rate of maximum missing values before a dimension is removed from
+	 * multivariate time series.
+	 */
+	private double missingValueRate;
+
+	@SuppressWarnings("unused")
+	private MissingValueDimensionsRemover() {
+		this(Double.NaN, 0.75);
+	}
+
+	public MissingValueDimensionsRemover(double missingValueIndicator, double missingValueRate) {
+		this.missingValueIndicator = missingValueIndicator;
+		this.missingValueRate = missingValueRate;
+	}
+
+	@Override
+	public void process(List<ITimeSeriesMultivariate> data) {
+		if (data.isEmpty())
+			throw new IllegalStateException("List<TimeSeries> is empty");
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i) == null)
+				throw new IllegalStateException("TimeSeries is null");
+			if (data.get(i).isEmpty())
+				throw new IllegalStateException("TimeSeries is empty");
+			process(data.get(i));
+		}
+	}
+
+	private void process(ITimeSeriesMultivariate data) {
+		for (int d = 0; d < data.getDimensionality(); d++) {
+			ITimeSeriesUnivariate timeSeries = data.getTimeSeries(d);
+
+			int count = 0;
+			for (Double value : timeSeries.getValues())
+				if (value != null && TimeSeriesTools.compareDoubles(value, missingValueIndicator))
+					count++;
+
+			if (count / (double) timeSeries.size() > missingValueRate)
+				data.removeTimeSeries(d--);
+		}
+	}
+
+	@Override
+	public DataProcessingCategory getPreprocessingCategory() {
+		return DataProcessingCategory.DATA_CLEANING;
+	}
+
+	@Override
+	public List<IDataProcessor<ITimeSeriesMultivariate>> getAlternativeParameterizations(int count) {
+		List<IDataProcessor<ITimeSeriesMultivariate>> alternatives = new ArrayList<>();
+
+		List<Double> alternativeDoubles = ParameterSupportTools.getAlternativeDoubles(missingValueRate, count);
+
+		for (Double v : alternativeDoubles) {
+			if (v >= 0 && v <= 1)
+				alternatives.add(new MissingValueDimensionsRemover(missingValueIndicator, v));
+			if (alternatives.size() == count)
+				return alternatives;
+		}
+
+		return alternatives;
+	}
+
+	public double getMissingValueRate() {
+		return missingValueRate;
+	}
+
+	public void setMissingValueRate(double missingValueRate) {
+		this.missingValueRate = missingValueRate;
+	}
+}
