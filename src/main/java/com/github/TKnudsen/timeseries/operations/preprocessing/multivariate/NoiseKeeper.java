9a30fc99550dc519670b16528c0675fdbca748c6
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-12-10 18:25:19 +0100

    added noise keeper routine

---
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/NoiseKeeper.java
@@ -0,0 +1,93 @@
+package com.github.TKnudsen.timeseries.operations.preprocessing.multivariate;
+
+import com.github.TKnudsen.ComplexDataObject.model.processors.IDataProcessor;
+import com.github.TKnudsen.ComplexDataObject.model.processors.ParameterSupportTools;
+import com.github.TKnudsen.ComplexDataObject.model.weighting.Integer.IIntegerWeightingKernel;
+import com.github.TKnudsen.ComplexDataObject.model.weighting.Integer.LinearIndexWeightingKernel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.TKnudsen.timeseries.data.multivariate.ITimeSeriesMultivariate;
+import com.github.TKnudsen.timeseries.operations.preprocessing.multivariate.dimensionBased.DimensionBasedTimeSeriesMultivariateProcessor;
+
+/**
+ * 
+ * timeSeries
+ *
+ * Copyright: (c) 2016-2018 Juergen Bernard,
+ * https://github.com/TKnudsen/timeSeries<br>
+ * <br>
+ * 
+ * removes the signal of a time series and preserves the noise. the signal is
+ * calculated with a moving average routine.
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * 
+ * @version 1.03
+ */
+public class NoiseKeeper extends DimensionBasedTimeSeriesMultivariateProcessor {
+
+	final IIntegerWeightingKernel kernel;
+
+	private boolean considerFutureValues = false;
+
+	/**
+	 * for serialization/reflection purposes.
+	 */
+	@SuppressWarnings("unused")
+	private NoiseKeeper() {
+		this(3, true);
+	}
+
+	public NoiseKeeper(int kernelInterval, boolean considerFutureValues) {
+		this(new LinearIndexWeightingKernel(kernelInterval), considerFutureValues);
+	}
+
+	public NoiseKeeper(IIntegerWeightingKernel kernel, boolean considerFutureValues) {
+		if (kernel == null)
+			throw new NullPointerException("MovingAverageTimeBased: kernel was null");
+
+		this.kernel = kernel;
+		this.considerFutureValues = considerFutureValues;
+	}
+
+	@Override
+	protected void initializeUnivariateTimeSeriesProcessor() {
+		this.setTimeSeriesProcessor(new com.github.TKnudsen.timeseries.operations.preprocessing.univariate.NoiseKeeper(
+				kernel, considerFutureValues));
+	}
+
+	@Override
+	public List<IDataProcessor<ITimeSeriesMultivariate>> getAlternativeParameterizations(int count) {
+		List<Integer> ints = ParameterSupportTools.getAlternativeIntegers(kernel.getInterval(), count);
+
+		List<IDataProcessor<ITimeSeriesMultivariate>> processors = new ArrayList<>();
+		for (Integer i : ints)
+			processors.add(new NoiseKeeper(i, considerFutureValues));
+
+		return processors;
+	}
+
+	public int getKernelInterval() {
+		return kernel.getInterval();
+	}
+
+	public void setKernelInterval(int kernelInterval) {
+		this.kernel.setInterval(kernelInterval);
+
+		initializeUnivariateTimeSeriesProcessor();
+	}
+
+	public boolean isConsiderFutureValues() {
+		return considerFutureValues;
+	}
+
+	public void setConsiderFutureValues(boolean considerFutureValues) {
+		this.considerFutureValues = considerFutureValues;
+
+		initializeUnivariateTimeSeriesProcessor();
+	}
+
+}
