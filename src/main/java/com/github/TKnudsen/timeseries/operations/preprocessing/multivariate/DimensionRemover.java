abb59b357fd4226fce19d0452a5de411099c571b
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2019-01-09 23:02:21 +0100

    added DimensionRemover (MVTS processor)

---
+++ src/main/java/com/github/TKnudsen/timeseries/operations/preprocessing/multivariate/DimensionRemover.java
@@ -0,0 +1,73 @@
+package com.github.TKnudsen.timeseries.operations.preprocessing.multivariate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.TKnudsen.ComplexDataObject.model.processors.IDataProcessor;
+import com.github.TKnudsen.ComplexDataObject.model.processors.complexDataObject.DataProcessingCategory;
+import com.github.TKnudsen.timeseries.data.multivariate.ITimeSeriesMultivariate;
+import com.github.TKnudsen.timeseries.operations.preprocessing.TimeSeriesProcessor;
+
+/**
+ * 
+ * TimeSeries
+ *
+ * Copyright: (c) 2015-2018 Juergen Bernard,
+ * https://github.com/TKnudsen/TimeSeries<br>
+ * 
+ * DimensionRemover is a pre-processing routine that removes the dimension with
+ * a given name
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
+public class DimensionRemover extends TimeSeriesProcessor<ITimeSeriesMultivariate> {
+
+	private final String dimensionName;
+
+	@SuppressWarnings("unused")
+	private DimensionRemover() {
+		this(null);
+	}
+
+	public DimensionRemover(String dimensionName) {
+		this.dimensionName = dimensionName;
+	}
+
+	@Override
+	public void process(List<ITimeSeriesMultivariate> data) {
+
+		if (data.isEmpty())
+			throw new IllegalStateException("List<TimeSeries> is empty");
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i) == null)
+				throw new IllegalStateException("TimeSeries is null");
+			if (data.get(i).isEmpty())
+				throw new IllegalStateException("TimeSeries is empty");
+			process(data.get(i));
+		}
+	}
+
+	private void process(ITimeSeriesMultivariate data) {
+		data.removeTimeSeries(getDimensionName());
+	}
+
+	@Override
+	public DataProcessingCategory getPreprocessingCategory() {
+		return DataProcessingCategory.DATA_REDUCTION;
+	}
+
+	@Override
+	public List<IDataProcessor<ITimeSeriesMultivariate>> getAlternativeParameterizations(int count) {
+		List<IDataProcessor<ITimeSeriesMultivariate>> alternatives = new ArrayList<>();
+
+		return alternatives;
+	}
+
+	public String getDimensionName() {
+		return dimensionName;
+	}
+
+}
